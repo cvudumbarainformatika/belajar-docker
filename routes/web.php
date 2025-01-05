9aebf337ac03b7ea239bfa953436f77e94c21879
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-01-05 11:18:25 +0700

    rev: supervisord

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/autogen', function () {
+
+    // $data = User::all();
+    // return response()->json($data);
+    echo 'ok';
 });
