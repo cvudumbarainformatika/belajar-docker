81afb2063b3dbe9ee8b2d5086bcd8effe94f2d32
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-01-05 14:13:00 +0700

    rev: watch not working

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return 111;
     return view('welcome');
 });
 
 Route::get('/autogen', function () {
 
-    $data = User::all();
-    return response()->json($data);
-    // echo 'ok';
+    // $data = User::all();
+    // return response()->json($data);
+    echo 'ok11';
 });
